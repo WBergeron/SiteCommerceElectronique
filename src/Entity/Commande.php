5b163b4c67601c534b30b9d23231eda05ae006bf
WBergeron <2052824@cstj.qc.ca>
2023-04-20 13:30:54 -0400

    Création de l'entity commande

---
+++ src/Entity/Commande.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommandeRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommandeRepository::class)]
+#[ORM\Table(
+    name: 'commande'
+
+
+
+
+
+
+)]
+class Commande
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    private ?int $idCommande = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateCommande = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateLivraison = null;
+
+    #[ORM\Column]
+    private ?float $tauxTPS = null;
+
+    #[ORM\Column]
+    private ?float $tauxTVQ = null;
+
+    #[ORM\Column]
+    private ?float $fraisLivraison = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $etat = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $stripeIntent = null;
+
+
+    public function getIdCommande(): ?int
+    {
+        return $this->idCommande;
+    }
+
+    public function getDateCommande(): ?\DateTimeInterface
+    {
+        return $this->dateCommande;
+    }
+
+    public function setDateCommande(\DateTimeInterface $dateCommande): self
+    {
+        $this->dateCommande = $dateCommande;
+
+        return $this;
+    }
+
+    public function getDateLivraison(): ?\DateTimeInterface
+    {
+        return $this->dateLivraison;
+    }
+
+    public function setDateLivraison(\DateTimeInterface $dateLivraison): self
+    {
+        $this->dateLivraison = $dateLivraison;
+
+        return $this;
+    }
+
+    public function getTauxTPS(): ?float
+    {
+        return $this->tauxTPS;
+    }
+
+    public function setTauxTPS(float $tauxTPS): self
+    {
+        $this->tauxTPS = $tauxTPS;
+
+        return $this;
+    }
+
+    public function getTauxTVQ(): ?float
+    {
+        return $this->tauxTVQ;
+    }
+
+    public function setTauxTVQ(float $tauxTVQ): self
+    {
+        $this->tauxTVQ = $tauxTVQ;
+
+        return $this;
+    }
+
+    public function getFraisLivraison(): ?float
+    {
+        return $this->fraisLivraison;
+    }
+
+    public function setFraisLivraison(float $fraisLivraison): self
+    {
+        $this->fraisLivraison = $fraisLivraison;
+
+        return $this;
+    }
+
+    public function getEtat(): ?string
+    {
+        return $this->etat;
+    }
+
+    public function setEtat(string $etat): self
+    {
+        $this->etat = $etat;
+
+        return $this;
+    }
+
+    public function getStripeIntent(): ?string
+    {
+        return $this->stripeIntent;
+    }
+
+    public function setStripeIntent(string $stripeIntent): self
+    {
+        $this->stripeIntent = $stripeIntent;
+
+        return $this;
+    }
+}
