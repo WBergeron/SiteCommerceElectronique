e90acb7b1fb14bbe298e8d38e3309c7766629971
William(2052824) <2052824@cstj.qc.ca>
2023-04-21 10:21:36 -0400

    Devellopement de la page commande

---
+++ src/Controller/CommandeController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Panier;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+
+use Stripe;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class CommandeController extends AbstractController
+{
+    private $panier;
+    private $em = null;
+
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->em = $doctrine->getManager();
+    }
+
+    #[Route('/commande', name: 'app_commande')]
+    public function index(Request $request): Response
+    {
+        // Revue
+        // Vérification d'acces
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $session = $request->getSession();
+        if (!$session) {
+            return $this->redirectToRoute('app_connection');
+        }
+
+        return $this->render('commande/commande.html.twig', [
+            'listAchat' => $this->panier
+        ]);
+    }
+
+    private function initSession(Request $request)
+    {
+        $session = $request->getSession();
+
+        $session->set('name', 'William');
+
+        $this->panier = $session->get('panier', new Panier());
+        $session->set('panier', $this->panier);
+    }
+}
